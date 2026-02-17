914a2dad5cdb0244cc249176280946d4b5f7c724
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-02-16 21:03:30 -0500

    Show message when executing cli if current version is outdated

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,19 @@
 import { build } from '@/builder'
-import { error } from '@/logger'
+import { error, info, warn } from '@/logger'
 import { getExtension, parse } from '@/parser'
 import { validateInput } from '@/verifier'
-import { showFlagsInfo } from './utils'
+import { version } from '../package.json'
+import { getLatestVersion, showFlagsInfo } from './utils'
 
 export const args = process.argv.slice(2)
 
 await showFlagsInfo(args)
+
+const latestVersion = await getLatestVersion()
+if (latestVersion && latestVersion.trim() !== version.trim()) {
+	warn(`✨ New version available: ${latestVersion}`)
+	info(`Run "npm install -g @cmorales_/touch" to update`)
+}
 
 const input = args[0]!.replace(/\s/g, '')
 const validation = validateInput(input)
