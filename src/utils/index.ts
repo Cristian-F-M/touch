449222038dff44877916ece0bd1370895d1a2877
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-03-16 21:08:23 -0500

    Add function to update cli

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
-import child_process from 'node:child_process'
+import child_process, { spawn } from 'node:child_process'
 import util from 'node:util'
 import { COMMANDS } from '@/constants/help'
+import pkg from '../../package.json' with { type: 'json' }
 
 export async function showFlagsInfo(args: string[]) {
 	for (const command of Object.values(COMMANDS)) {
@@ -20,4 +21,20 @@ export async function getLatestVersion() {
 	const { name } = await import('../../package.json')
 	const { stdout } = await exec(`npm view ${name} version`)
 	return stdout.toString().trim()
+}
+
+export function updateCli() {
+	const { promise, resolve, reject } = Promise.withResolvers<void>()
+
+	const child = spawn('npm', ['install', '-g', pkg.name], {
+		stdio: 'inherit',
+		shell: true
+	})
+
+	child.on('close', (code) => {
+		if (code === 0) resolve()
+		else reject(new Error(`Update failed with code ${code}`))
+	})
+
+	return promise
 }
