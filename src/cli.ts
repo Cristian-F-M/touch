7455e5ffb79cfd36707be1c5a591e8c44e5f552d
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-05-30 11:37:03 -0500

    Refactor logic to use args parser, refactor logic to show when package is outdated

--- src/cli.ts
+++ src/cli.ts
@@ -1,44 +1,56 @@
+import util from 'node:util'
 import updateNotifier from 'update-notifier'
 import { build } from '@/builder'
-import { error } from '@/logger'
+import { parseArgsConfig } from '@/constants/args-parser'
+import { DOCS_URL, HELP_MESSAGE } from '@/constants/help'
+import { error, log } from '@/logger'
 import { getExtension, parse } from '@/parser'
+import { handleUpdateCli, onFetchInfoSucces } from '@/utils'
 import { validateInput } from '@/verifier'
 import pkg from '../package.json' with { type: 'json' }
-import { onFetchInfoSucces, showFlagsInfo } from './utils'
 
 let fetched = false
 
+process.on('beforeExit', async () => {
+	if (fetched) return
+
+	const notifier = updateNotifier({
+		pkg,
+		updateCheckInterval: 60 * 60 * 24 * 2
+	})
+	const info = await notifier.fetchInfo()
+	fetched = true
+
+	onFetchInfoSucces(info)
+})
+
 async function main() {
-	const args = process.argv.slice(2)
+	const { values, positionals } = util.parseArgs(parseArgsConfig)
 
-	const handled = await showFlagsInfo(args)
+	if (values.help) return console.log(HELP_MESSAGE)
+	if (values.docs) return log(`To see the documentation, visit: ${DOCS_URL}`)
 
-	if (!handled) {
-		const input = args[0]!.replace(/\s/g, '')
-		const validation = validateInput(input)
+	if (values.version) {
+		const { name, version } = pkg
+		log(`${name} v${version}`)
+		return
+	}
+	if (values.upgrade) return handleUpdateCli()
 
-		if (!validation.valid) {
-			error(validation.error!)
-			process.exit(1)
-		}
+	if (!positionals.length) return error('No input and no flags provided')
 
-		const tree = parse(input)
-		const ext = getExtension(input)
+	const input = positionals[0]!.replace(/\s/g, '')
+	const validation = validateInput(input)
 
-		build(tree, process.cwd(), ext)
+	if (!validation.valid) {
+		error(validation.error!)
+		return
 	}
 
-	if (!fetched) {
-		const notifier = updateNotifier({
-			pkg,
-			updateCheckInterval: 0
-		})
+	const tree = parse(input)
+	const ext = getExtension(input)
 
-		const info = await notifier.fetchInfo()
-		fetched = true
-
-		onFetchInfoSucces(info)
-	}
+	build(tree, process.cwd(), ext)
 }
 
 main()
